37a2fc42f939f3dc83f1b2d88170f1229a37be6f
flosacca <flosacca@gmail.com>
2020-09-09 17:56:56 +0800

    Rewrite User

--- users/models.py
+++ users/models.py
@@ -1,14 +1,36 @@
-from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+import bcrypt
 
 
-class User(AbstractBaseUser):
+class User(models.Model):
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(default='', max_length=255)
-    address = models.CharField(default='', max_length=255)
-    contact = models.CharField(default='', max_length=255)
+    name = models.CharField(max_length=255, default='')
+    address = models.CharField(max_length=255, default='')
+    contact = models.CharField(max_length=255, default='')
 
-    USERNAME_FIELD = 'email'
+    # password hash
+    password = models.BinaryField()
+
+    group = models.CharField(max_length=255, default='user')
+
+    @classmethod
+    def create(cls, email, password, **kwargs):
+        pw = password.encode('utf-8')
+        return cls.objects.create(**kwargs, **{
+            'email': email,
+            'password': bcrypt.hashpw(pw, bcrypt.gensalt())
+        })
+
+    @classmethod
+    def from_session(cls, session):
+        if session is None:
+            return None
+        pk = session.get_decoded()['user_id']
+        return cls.objects.filter(pk=pk).first()
 
     def __str__(self):
         return self.email
+
+    def authenticate(self, password):
+        pw = password.encode('utf-8')
+        return bcrypt.checkpw(pw, self.password)
